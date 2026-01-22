76f5ceb3d0d9aaca847c54be068b20024e52706c
Menaka J <menakaraman4@gmail.com>
2026-01-22 16:26:58 +0530

    add g&s

--- src/main/java/com/menaka/ecart/entity/Product.java
+++ src/main/java/com/menaka/ecart/entity/Product.java
@@ -11,4 +11,81 @@ public class Product {
     private Integer stock;
     private Integer numOfReviews = 0;
 
+    //constructor for initialization
+    public Product(String seller, Long id, String name, Double price, String description, Double ratings, Integer stock, Integer numOfReviews) {
+        this.seller = seller;
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.ratings = ratings;
+        this.stock = stock;
+        this.numOfReviews = numOfReviews;
+    }
+
+    //getter and setter for all columns
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(Double ratings) {
+        this.ratings = ratings;
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public void setSeller(String seller) {
+        this.seller = seller;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Integer getNumOfReviews() {
+        return numOfReviews;
+    }
+
+    public void setNumOfReviews(Integer numOfReviews) {
+        this.numOfReviews = numOfReviews;
+    }
 }
