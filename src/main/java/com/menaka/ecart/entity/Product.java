3bbbd20a6dddc6aa77dfe8b836e6d4d72734ef19
Menaka J <menakaraman4@gmail.com>
2026-01-22 16:24:43 +0530

    add entity col

--- src/main/java/com/menaka/ecart/entity/Product.java
+++ src/main/java/com/menaka/ecart/entity/Product.java
@@ -1,4 +1,14 @@
 package com.menaka.ecart.entity;
 
 public class Product {
+
+    private Long id;
+    private String name;
+    private Double price;
+    private String description;
+    private Double ratings = 0.0;
+    private String seller;
+    private Integer stock;
+    private Integer numOfReviews = 0;
+
 }
