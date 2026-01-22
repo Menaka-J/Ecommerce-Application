7556c82c8760db44873ffc1f29922dd98db0f8ea
Menaka J <menakaraman4@gmail.com>
2026-01-22 16:42:06 +0530

    add image col

--- src/main/java/com/menaka/ecart/entity/ProductImage.java
+++ src/main/java/com/menaka/ecart/entity/ProductImage.java
@@ -1,4 +1,17 @@
 package com.menaka.ecart.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class ProductImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String publicId;
+    private String url;
 }
