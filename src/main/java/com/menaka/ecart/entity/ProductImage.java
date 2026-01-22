b814b91af982c3376ff47bba05cac44bae91c19b
Menaka J <menakaraman4@gmail.com>
2026-01-22 16:44:46 +0530

    add image g&s

--- src/main/java/com/menaka/ecart/entity/ProductImage.java
+++ src/main/java/com/menaka/ecart/entity/ProductImage.java
@@ -26,5 +26,29 @@ public class ProductImage {
 
     }
 
+    //getter and setter
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPublicId() {
+        return publicId;
+    }
+
+    public void setPublicId(String publicId) {
+        this.publicId = publicId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
