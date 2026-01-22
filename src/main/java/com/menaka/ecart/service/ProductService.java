767f3eca64c68f7016cb90868fd43eb0768300ca
Menaka J <menakaraman4@gmail.com>
2026-01-22 17:15:00 +0530

    add service1

--- src/main/java/com/menaka/ecart/service/ProductService.java
+++ src/main/java/com/menaka/ecart/service/ProductService.java
@@ -1,4 +1,18 @@
 package com.menaka.ecart.service;
 
+import com.menaka.ecart.entity.Product;
+import com.menaka.ecart.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+
+    private ProductRepository productRepository;
+
+    public List<Product> getALlProducte() {
+        List<Product> products = productRepository.findAll();
+        return products;
+    }
 }
