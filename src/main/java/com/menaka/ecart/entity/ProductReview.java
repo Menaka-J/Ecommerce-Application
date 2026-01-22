2d9583e88e727aced7c9d40b560f493c349b4552
Menaka J <menakaraman4@gmail.com>
2026-01-22 16:47:14 +0530

    add review col

--- src/main/java/com/menaka/ecart/entity/ProductReview.java
+++ src/main/java/com/menaka/ecart/entity/ProductReview.java
@@ -1,4 +1,24 @@
 package com.menaka.ecart.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
+@Entity
 public class ProductReview {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Min(value = 1)
+    @Max(value = 5)
+    private Integer rating;
+
+    private String comment;
+
+
 }
