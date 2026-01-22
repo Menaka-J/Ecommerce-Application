81f92069fec2c666391d2c59c89b3d0d2fc2a25e
Menaka J <menakaraman4@gmail.com>
2026-01-22 17:36:02 +0530

    add controller

--- src/main/java/com/menaka/ecart/EcartApplication.java
+++ src/main/java/com/menaka/ecart/EcartApplication.java
@@ -2,12 +2,18 @@ package com.menaka.ecart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class EcartApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcartApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EcartApplication.class, args);
+    }
 
 }
+
+//HINTS:
+
+//THE BELOW LINE USES exclude WHICH TEMPORARILY STOP SECURITY[IF ADDED IN DEPENDENCIDE]
+//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
