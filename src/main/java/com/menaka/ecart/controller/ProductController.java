1ea350a6dca4f06acaa73f73940dad460aab1851
Menaka J <menakaraman4@gmail.com>
2026-01-22 17:23:33 +0530

    add controller

--- src/main/java/com/menaka/ecart/controller/ProductController.java
+++ src/main/java/com/menaka/ecart/controller/ProductController.java
@@ -1,4 +1,11 @@
 package com.menaka.ecart.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+
+
 }
