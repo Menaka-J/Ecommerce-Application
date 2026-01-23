31b01b7c8dc0bda8e0467c22104029ad77aee2eb
Menaka J <menakaraman4@gmail.com>
2026-01-23 20:17:08 +0530

    add search

--- src/main/java/com/menaka/ecart/controller/ProductReviewController.java
+++ src/main/java/com/menaka/ecart/controller/ProductReviewController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class ProductReviewController {
     private ProductService productService;
 
     //for adding review
+    @PostMapping
     public ResponseEntity<?> addReview(@RequestBody ProductReviewDto reviewDto) {
         productService.addReview(reviewDto);
         return ResponseEntity.status(HttpStatus.CREATED).body("Review added");
