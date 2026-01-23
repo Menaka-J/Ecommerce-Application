5badf926642cf125c9c0d85012f04ba6a9b92cfd
Menaka J <menakaraman4@gmail.com>
2026-01-23 19:52:34 +0530

    add search

--- src/main/java/com/menaka/ecart/controller/ProductReviewController.java
+++ src/main/java/com/menaka/ecart/controller/ProductReviewController.java
@@ -1,4 +1,9 @@
 package com.menaka.ecart.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products/reviews")
 public class ProductReviewController {
 }
