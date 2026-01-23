d2aca97af743e2fe445d8faff7eda71d755ace0f
Menaka J <menakaraman4@gmail.com>
2026-01-23 17:22:30 +0530

    add seed1

--- src/main/java/com/menaka/ecart/repository/ProductRepository.java
+++ src/main/java/com/menaka/ecart/repository/ProductRepository.java
@@ -4,7 +4,6 @@ import com.menaka.ecart.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
-@Component
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
--- src/main/java/com/menaka/ecart/seed/ProductSeeder.java
+++ src/main/java/com/menaka/ecart/seed/ProductSeeder.java
@@ -4,10 +4,12 @@ import com.menaka.ecart.entity.Product;
 import com.menaka.ecart.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 //for inserting demo data
+@Component
 public class ProductSeeder implements CommandLineRunner {
 
     @Autowired
