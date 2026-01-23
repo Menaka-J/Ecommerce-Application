6a6a913dc02522b5a06b5ae90914f087bc43fc8b
Menaka J <menakaraman4@gmail.com>
2026-01-23 18:16:11 +0530

    add seed

--- src/main/java/com/menaka/ecart/repository/ProductRepository.java
+++ src/main/java/com/menaka/ecart/repository/ProductRepository.java
@@ -2,8 +2,9 @@ package com.menaka.ecart.repository;
 
 import com.menaka.ecart.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Component;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
 
 }
