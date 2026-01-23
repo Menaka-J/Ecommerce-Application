05984f8da66c31dba7af01061f35a1bb942cbb8c
Menaka J <menakaraman4@gmail.com>
2026-01-23 17:52:35 +0530

    add seed

--- src/main/java/com/menaka/ecart/controller/ProductController.java
+++ src/main/java/com/menaka/ecart/controller/ProductController.java
@@ -4,10 +4,7 @@ import com.menaka.ecart.entity.Product;
 import com.menaka.ecart.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -23,6 +20,12 @@ public class ProductController {
     @GetMapping
     public Map<String, Object> getAllProducts(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size) {
         return productService.getAllProducts(page, size);
+    }
+
+    //GETTING PRODUCT BY ID
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
     }
 
 }
