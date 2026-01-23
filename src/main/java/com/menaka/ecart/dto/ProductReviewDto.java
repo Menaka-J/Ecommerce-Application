2759abf7abab8ea9bc054605ae72b2afb84ab65d
Menaka J <menakaraman4@gmail.com>
2026-01-23 19:58:17 +0530

    add search

--- src/main/java/com/menaka/ecart/dto/ProductReviewDto.java
+++ src/main/java/com/menaka/ecart/dto/ProductReviewDto.java
@@ -1,4 +1,35 @@
 package com.menaka.ecart.dto;
 
 public class ProductReviewDto {
+
+    private Long productId;
+    private String comment;
+    private Double rating;
+
+    //getter and setter
+
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
 }
