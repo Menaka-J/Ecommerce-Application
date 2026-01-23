404c8a74834220aa4481ef6479da8e4cf45df7df
Menaka J <menakaraman4@gmail.com>
2026-01-23 20:14:22 +0530

    add search

--- src/main/java/com/menaka/ecart/repository/ProductReviewRepository.java
+++ src/main/java/com/menaka/ecart/repository/ProductReviewRepository.java
@@ -1,4 +1,8 @@
 package com.menaka.ecart.repository;
 
-public class ProductReviewRepository {
+import com.menaka.ecart.entity.ProductReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductReviewRepository extends JpaRepository<ProductReview, Long> {
+
 }
