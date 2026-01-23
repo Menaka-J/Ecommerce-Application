1672b2ff9dc3dc6caff6b1c4422d4cbe776bc621
Menaka J <menakaraman4@gmail.com>
2026-01-23 18:17:07 +0530

    add seed

--- src/main/java/com/menaka/ecart/service/ProductService.java
+++ src/main/java/com/menaka/ecart/service/ProductService.java
@@ -7,14 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Pageable;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 @Service
 public class ProductService {
@@ -40,8 +38,9 @@ public class ProductService {
     }
 
     //FOR SEARCHING PRODUCTS
-    public void filterProducts(String category, double minPrice, double maxPrice, String Keyword) {
+    public List<Product> searchProducts(String category, double minPrice, double maxPrice, String Keyword) {
         Specification<Product> spec = Specification.where(ProductSpecification.hasCategory(category));
+        return productRepository.findAll(spec);
     }
 
 
