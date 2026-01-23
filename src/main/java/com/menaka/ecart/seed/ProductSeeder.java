e3e04b612f0ef891d297279f67f4887d304fe7db
Menaka J <menakaraman4@gmail.com>
2026-01-23 17:10:06 +0530

    add seed1

--- src/main/java/com/menaka/ecart/seed/ProductSeeder.java
+++ src/main/java/com/menaka/ecart/seed/ProductSeeder.java
@@ -5,7 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 //for inserting demo data
 public class ProductSeeder implements CommandLineRunner {
 
-
+    //Runs first when we start application[so data will be stored in database]
     @Override
     public void run(String... args) throws Exception {
 
