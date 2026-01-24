74250968fa6a0f97d7edae102cb84bc195ac0bda
Menaka J <menakaraman4@gmail.com>
2026-01-24 15:25:18 +0530

    add images

--- src/main/java/com/menaka/ecart/entity/Order.java
+++ src/main/java/com/menaka/ecart/entity/Order.java
@@ -1,12 +1,17 @@
 package com.menaka.ecart.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import java.util.List;
 
 @Entity
 public class Order {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private List<OrderItem> orderItems;
     private Double totalItemsAmount;
     private Double taxAmount;
