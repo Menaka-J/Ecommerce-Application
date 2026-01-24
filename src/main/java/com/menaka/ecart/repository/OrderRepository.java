bb7e67a87fbb59a212dd64e82449b765b900ca44
Menaka J <menakaraman4@gmail.com>
2026-01-24 15:50:28 +0530

    add orderrepo

--- src/main/java/com/menaka/ecart/repository/OrderRepository.java
+++ src/main/java/com/menaka/ecart/repository/OrderRepository.java
@@ -1,4 +1,7 @@
 package com.menaka.ecart.repository;
 
-public interface OrderRepository {
+import com.menaka.ecart.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
