38cac10b183a24a894526aa5abd19795067abb48
Menaka J <menakaraman4@gmail.com>
2026-01-24 14:42:41 +0530

    add images

--- src/main/java/com/menaka/ecart/seed/ProductSeeder.java
+++ src/main/java/com/menaka/ecart/seed/ProductSeeder.java
@@ -21,14 +21,14 @@ public class ProductSeeder implements CommandLineRunner {
 
         if (productRepository.count() == 0) {
             List<Product> demoProducts = List.of(
-                    new Product(null, "MacBook Air M3", 1099.00, "Lightweight powerhouse laptop", "Laptops", 4.8, "Apple Store", 10),
-                    new Product(null, "Sony WH-1000XM5", 348.00, "Noise cancelling headphones", "Audio", 4.5, "Best Buy", 25),
-                    new Product(null, "Samsung Galaxy Watch 6", 299.99, "Smartwatch with health tracking", "Wearables", 4.6, "Samsung", 15),
-                    new Product(null, "Keychron K2 Pro", 89.00, "Wireless mechanical keyboard", "Accessories", 4.7, "Keychron", 50),
-                    new Product(null, "Dell UltraSharp Monitor", 579.50, "4K USB-C Hub Monitor", "Monitors", 4.4, "Dell", 8),
-                    new Product(null, "Logitech MX Master 3S", 99.99, "Ergonomic performance mouse", "Accessories", 4.9, "Logitech", 40),
-                    new Product(null, "Kindle Paperwhite", 139.99, "Waterproof e-reader", "Tablets", 4.8, "Amazon", 30),
-                    new Product(null, "JBL Flip 6", 129.95, "Portable waterproof speaker", "Audio", 4.7, "JBL Store", 20)
+                    new Product(null, "MacBook Air M3", 1099.00, "Lightweight powerhouse laptop", "Laptops", 4.8, "Apple Store", 10, List.of("/products/1ig.jpg")),
+                    new Product(null, "Sony WH-1000XM5", 348.00, "Noise cancelling headphones", "Audio", 4.5, "Best Buy", 25, List.of("/products/2ig.jpg")),
+                    new Product(null, "Samsung Galaxy Watch 6", 299.99, "Smartwatch with health tracking", "Wearables", 4.6, "Samsung", 15, List.of("/products/3ig.jpg")),
+                    new Product(null, "Keychron K2 Pro", 89.00, "Wireless mechanical keyboard", "Accessories", 4.7, "Keychron", 50, List.of("/products/4ig.jpg")),
+                    new Product(null, "Dell UltraSharp Monitor", 579.50, "4K USB-C Hub Monitor", "Monitors", 4.4, "Dell", 8, List.of("/products/5ig.jpg")),
+                    new Product(null, "Logitech MX Master 3S", 99.99, "Ergonomic performance mouse", "Accessories", 4.9, "Logitech", 40, List.of("/products/6ig.jpg")),
+                    new Product(null, "Kindle Paperwhite", 139.99, "Waterproof e-reader", "Tablets", 4.8, "Amazon", 30, List.of("/products/7ig.jpg")),
+                    new Product(null, "JBL Flip 6", 129.95, "Portable waterproof speaker", "Audio", 4.7, "JBL Store", 20, List.of("/products/8ig.jpg"))
             );
 
             productRepository.saveAll(demoProducts);
