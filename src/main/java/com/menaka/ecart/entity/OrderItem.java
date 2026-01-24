e3e0581de9517f6e7e6c0fa4a5fc38d052fd3347
Menaka J <menakaraman4@gmail.com>
2026-01-24 15:21:27 +0530

    add images

--- src/main/java/com/menaka/ecart/entity/OrderItem.java
+++ src/main/java/com/menaka/ecart/entity/OrderItem.java
@@ -1,4 +1,10 @@
 package com.menaka.ecart.entity;
 
 public class OrderItem {
+
+    private String name;
+    private Integer quantity;
+    private String image;
+    private Double price;
+    private Product product;
 }
