cb7b81443b04e6e38b55b9c08090a52b06dd4e11
Menaka J <menakaraman4@gmail.com>
2026-01-24 15:51:13 +0530

    add orderrepo

--- src/main/java/com/menaka/ecart/service/OrderService.java
+++ src/main/java/com/menaka/ecart/service/OrderService.java
@@ -5,6 +5,7 @@ import com.menaka.ecart.dto.OrderItemDto;
 import com.menaka.ecart.entity.Order;
 import com.menaka.ecart.entity.OrderItem;
 import com.menaka.ecart.entity.Product;
+import com.menaka.ecart.repository.OrderRepository;
 import com.menaka.ecart.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,9 @@ public class OrderService {
 
     @Autowired
     private ProductRepository prodRepo;
+
+    @Autowired
+    private OrderRepository orderRepo;
 
     public void createOrder(CreateOrderRequest orderRequest) {
         Order order = new Order();
