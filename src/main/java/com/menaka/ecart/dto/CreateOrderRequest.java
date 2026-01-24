41945f572595e8c60dfec3eec21e84123810eae8
Menaka J <menakaraman4@gmail.com>
2026-01-24 15:42:51 +0530

    add order

--- src/main/java/com/menaka/ecart/dto/CreateOrderRequest.java
+++ src/main/java/com/menaka/ecart/dto/CreateOrderRequest.java
@@ -4,4 +4,14 @@ import java.util.List;
 
 public class CreateOrderRequest {
     private List<OrderItemDto> orderItems;
+
+    //getter and setter
+
+    public List<OrderItemDto> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItemDto> orderItems) {
+        this.orderItems = orderItems;
+    }
 }
--- src/main/java/com/menaka/ecart/service/OrderService.java
+++ src/main/java/com/menaka/ecart/service/OrderService.java
@@ -1,4 +1,30 @@
 package com.menaka.ecart.service;
 
+import com.menaka.ecart.dto.CreateOrderRequest;
+import com.menaka.ecart.dto.OrderItemDto;
+import com.menaka.ecart.entity.Order;
+import com.menaka.ecart.entity.OrderItem;
+import com.menaka.ecart.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
+
+    @Autowired
+    private ProductRepository prodRepo;
+
+    public void createOrder(CreateOrderRequest orderRequest) {
+        Order order = new Order();
+
+        for (OrderItemDto item : orderRequest.getOrderItems()) {
+            OrderItem orderItem = new OrderItem();
+            orderItem.setName(item.getName());
+            orderItem.setPrice(item.getPrice());
+            orderItem.setImage(item.getImage());
+            orderItem.setQuantity(item.getQuantity());
+        }
+    }
+
+
 }
