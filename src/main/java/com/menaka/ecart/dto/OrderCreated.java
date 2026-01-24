06bbda30fcad9bef4649f83eb536d2febf735ea3
Menaka J <menakaraman4@gmail.com>
2026-01-24 16:21:24 +0530

    add getorder

--- src/main/java/com/menaka/ecart/dto/OrderCreated.java
+++ src/main/java/com/menaka/ecart/dto/OrderCreated.java
@@ -1,4 +1,15 @@
 package com.menaka.ecart.dto;
 
 public class OrderCreated {
+    private String referenceId;
+
+    //getter and setter
+
+    public String getReferenceId() {
+        return referenceId;
+    }
+
+    public void setReferenceId(String referenceId) {
+        this.referenceId = referenceId;
+    }
 }
