e0184b8f0d48830eb1eb7d7286dc72922492300f
Menaka J <menakaraman4@gmail.com>
2026-01-24 14:59:59 +0530

    add images

--- src/main/java/com/menaka/ecart/dto/ProductImageDto.java
+++ src/main/java/com/menaka/ecart/dto/ProductImageDto.java
@@ -1,4 +1,23 @@
 package com.menaka.ecart.dto;
 
 public class ProductImageDto {
+
+    private String url;
+
+    //constructor
+
+    public ProductImageDto(String url) {
+        this.url = url;
+    }
+
+    //getter and setter
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
 }
