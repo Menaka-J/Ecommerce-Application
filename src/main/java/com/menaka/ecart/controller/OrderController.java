20a08fbdf2757aef9954a65dff769b09800b59a6
Menaka J <menakaraman4@gmail.com>
2026-01-24 15:32:32 +0530

    add order

--- src/main/java/com/menaka/ecart/controller/OrderController.java
+++ src/main/java/com/menaka/ecart/controller/OrderController.java
@@ -1,4 +1,13 @@
 package com.menaka.ecart.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
 public class OrderController {
+
+    public ResponseEntity<?> createOrder(@RequestBody )
 }
