91fe30ce099d0dcbb514b0f9306272ccd4446954
Menaka J <menakaraman4@gmail.com>
2026-01-24 15:33:48 +0530

    add order

--- src/main/java/com/menaka/ecart/dto/CreateOrderRequest.java
+++ src/main/java/com/menaka/ecart/dto/CreateOrderRequest.java
@@ -1,4 +1,13 @@
 package com.menaka.ecart.dto;
 
 public class CreateOrderRequest {
+
+    private String name;
+    private Integer quantity;
+    private String image;
+    private Double price;
+    private Long productId;
+
+    //getter and settet
+
 }
